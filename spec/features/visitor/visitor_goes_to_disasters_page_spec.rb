94873fe53efc5a350b8b6bf82f438eef45c84420
VadLusk <AdLusk@gmail.com>
2018-05-25 13:39:15 -0600

    Add map script to disasters index. Add main.sass and initial css for main, *, and body.

--- spec/features/visitor/visitor_goes_to_disasters_page_spec.rb
+++ spec/features/visitor/visitor_goes_to_disasters_page_spec.rb
@@ -4,6 +4,6 @@ describe 'visitor goes to the disasters page and' do
   it 'sees a map with pins in it' do
     visit '/disasters'
 
-    expect(page).to have_css('.map')
+    expect(page).to have_css('#map')
   end
 end
