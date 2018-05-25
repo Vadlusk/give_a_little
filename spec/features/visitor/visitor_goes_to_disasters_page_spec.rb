6156a015abf1fdc76a8d80bf515f2f0d394f9f7f
VadLusk <AdLusk@gmail.com>
2018-05-24 18:25:21 -0600

    Add visitor goes to disasters page test.

---
+++ spec/features/visitor/visitor_goes_to_disasters_page_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe 'visitor goes to the disasters page and' do
+  it 'sees a map with pins in it' do
+    visit '/disasters'
+
+    expect(page).to have_css('.map')
+  end
+end
