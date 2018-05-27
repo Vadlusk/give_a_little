65e2346780dc45619d518abc4f1134b7d1a124ce
VadLusk <AdLusk@gmail.com>
2018-05-27 10:09:27 -0600

    Add one more test.

--- spec/features/visitor/visitor_clicks_on_donate_spec.rb
+++ spec/features/visitor/visitor_clicks_on_donate_spec.rb
@@ -5,7 +5,15 @@ describe 'visitor clicks on donate now' do
     visit disaster_path(45829)
     click_on 'Donate Now'
   end
+  scenario 'and sees the correct path' do
+    expect(current_path).to eq('/charities')
+  end
   scenario 'and sees a list of associated charities to select from' do
-
+    expect(page).to have_css('.charities')
+    within('.charities') do
+      expect(page).to have_css('.charity-name')
+      expect(page).to have_css('.rating')
+      expect(page).to have_css('.cause-img')
+    end
   end
 end
