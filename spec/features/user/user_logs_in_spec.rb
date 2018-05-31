77e985e75b4930553e21853bbb5d262fa959ef44
VadLusk <AdLusk@gmail.com>
2018-05-31 16:01:39 -0600

    Refactor test with fill_in_credentials.

--- spec/features/user/user_logs_in_spec.rb
+++ spec/features/user/user_logs_in_spec.rb
@@ -3,16 +3,13 @@ require 'rails_helper'
 describe 'user logs in' do
   let(:user) { create(:user) }
   before(:each) do
-
     visit dashboard_path
     click_on 'Log in'
   end
   scenario 'and sees their dashboard' do
     expect(current_path).to eq('/login')
 
-    fill_in 'email', with: user.email
-    fill_in 'password', with: user.password
-    click_on 'Log In'
+    fill_in_credentials(user.email, user.password)
 
     expect(current_path).to eq('/dashboard')
     expect(page).to have_link('Log out')
@@ -20,22 +17,24 @@ describe 'user logs in' do
   end
   context 'doesn\'t work when' do
     scenario 'the email isn\'t in my database already' do
-      message = "Email doesn't exist. Please create an account or try again."
+      message = 'Email doesn\'t exist. Please create an account or try again.'
 
-      fill_in 'email', with: 'not@real.com'
-      fill_in 'password', with: user.password
-      click_on 'Log In'
+      fill_in_credentials('not@real.com', user.password)
 
       expect(page).to have_content(message)
     end
     scenario 'the password isn\'t correct' do
-      message = "Incorrect password. Please try again."
+      message = 'Incorrect password. Please try again.'
 
-      fill_in 'email', with: user.email
-      fill_in 'password', with: 'nope'
-      click_on 'Log In'
+      fill_in_credentials(user.email, 'nope')
 
       expect(page).to have_content(message)
     end
   end
+end
+
+def fill_in_credentials(email, password)
+  fill_in 'email', with: email
+  fill_in 'password', with: password
+  click_on 'Log In'
 end
