a73f57307102ea6b0b61c6401ff9c853bedd8f95
VadLusk <AdLusk@gmail.com>
2018-05-31 14:12:23 -0600

    Add log in link to visitor nav.

--- spec/features/user/user_logs_in_spec.rb
+++ spec/features/user/user_logs_in_spec.rb
@@ -4,13 +4,14 @@ describe 'user logs in' do
   it 'and sees their dashboard' do
     user = create(:user)
 
+    visit dashboard_path
     click_on 'Log in'
 
     expect(current_path).to eq('/login')
 
-    fill_in 'user[email]', with: 'New@email.com'
-    fill_in 'user[password]', with: 'password'
-    click_on 'Log in'
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    click_on 'Submit'
 
     expect(current_path).to eq('/dashboard')
     expect(page).to have_link('Log out')
