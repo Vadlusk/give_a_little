7027197e35b4296099af27467a76b84d9c8eec05
VadLusk <AdLusk@gmail.com>
2018-05-30 19:19:02 -0600

    Add get to /register route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :disasters, only: %i[index show]
   resources :charities, only: %i[index]
   resources :donations, only: %i[new]
-  
-  get '/login', to: 'sessions#new'
+
+  get '/login',    to: 'sessions#new'
+  get '/register', to: 'users#new'
 end
