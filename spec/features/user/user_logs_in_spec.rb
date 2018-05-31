fa2def0ec4e981a8430e9e7b109fa3e811569e5e
VadLusk <AdLusk@gmail.com>
2018-05-31 16:21:36 -0600

    Fix test so user is actually going through login process. Move helper mthd to rails helper.

--- spec/features/user/user_logs_in_spec.rb
+++ spec/features/user/user_logs_in_spec.rb
@@ -32,9 +32,3 @@ describe 'user logs in' do
     end
   end
 end
-
-def fill_in_credentials(email, password)
-  fill_in 'email', with: email
-  fill_in 'password', with: password
-  click_on 'Log In'
-end
--- spec/features/user/user_logs_out_spec.rb
+++ spec/features/user/user_logs_out_spec.rb
@@ -4,11 +4,12 @@ describe 'user logs out' do
   it 'is possible' do
     user = create(:user)
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
     visit dashboard_path
+    click_on 'Log in'
+    fill_in_credentials(user.email, user.password)
     click_on 'Log out'
-    
+
     expect(page).to have_link('Create an Account')
+    expect(page).to_not have_link('Log out')
   end
 end
