d6a0341a889ef44fa60d4c40a6f5a4baa10b97b0
VadLusk <AdLusk@gmail.com>
2018-05-31 08:17:52 -0600

    Add test for logging out.

---
+++ spec/features/user/user_logs_out_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'user logs out' do
+  it 'is possible' do
+    user = create(:user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    click_on 'Log out'
+
+    expect(page).to have_link('Log in')
+    expect(page).to have_link('Create an Account')
+  end
+end
