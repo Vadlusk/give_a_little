c3940c5e3d698a5e2011a13bf0c5e409de42f7c4
VadLusk <AdLusk@gmail.com>
2018-05-31 08:21:09 -0600

    Fix test. Add visit a page.

--- spec/features/user/user_logs_out_spec.rb
+++ spec/features/user/user_logs_out_spec.rb
@@ -5,6 +5,8 @@ describe 'user logs out' do
     user = create(:user)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit dashboard_path
     click_on 'Log out'
 
     expect(page).to have_link('Log in')
