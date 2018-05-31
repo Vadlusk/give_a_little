2564fd90c596f837bea0e13f25de348c766f5d12
VadLusk <AdLusk@gmail.com>
2018-05-31 15:57:00 -0600

    Add two sad path tests. Change find by password to email.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,12 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    @user = User.find_by(password: params[:password])
-    if @user.nil?
-      && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect_to dashboard_path
-    end
+    @user = User.find_by(email: params[:email])
+    # if @user.nil?
+    #   && @user.authenticate(params[:password])
+    #   session[:user_id] = @user.id
+    #   redirect_to dashboard_path
+    # end
   end
 
   def destroy
--- spec/features/user/user_logs_in_spec.rb
+++ spec/features/user/user_logs_in_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe 'user logs in' do
+  let(:user) { create(:user) }
   before(:each) do
-    user = create(:user)
 
     visit dashboard_path
     click_on 'Log in'
@@ -20,7 +20,22 @@ describe 'user logs in' do
   end
   context 'doesn\'t work when' do
     scenario 'the email isn\'t in my database already' do
-      
+      message = "Email doesn't exist. Please create an account or try again."
+
+      fill_in 'email', with: 'not@real.com'
+      fill_in 'password', with: user.password
+      click_on 'Log In'
+
+      expect(page).to have_content(message)
+    end
+    scenario 'the password isn\'t correct' do
+      message = "Incorrect password. Please try again."
+
+      fill_in 'email', with: user.email
+      fill_in 'password', with: 'nope'
+      click_on 'Log In'
+
+      expect(page).to have_content(message)
     end
   end
 end
