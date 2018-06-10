4cf4d9a883f4e2ef4abf65659b6b6a720fedff70
VadLusk <AdLusk@gmail.com>
2018-06-09 21:57:09 -0600

    Add welcome method and body.

--- app/mailers/welcome_local_user_mailer.rb
+++ app/mailers/welcome_local_user_mailer.rb
@@ -1,2 +1,7 @@
 class WelcomeLocalUserMailer < ApplicationMailer
+  def welcome(user)
+    @user = user
+    mail(to: user,
+         subject: "Welcome to GiveALittle, #{user.first_name}!")
+  end
 end
